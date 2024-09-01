f4925f23629b37544df1ddbe7d23e66f538adde0
Aughostinos <augustinos.nabil@gmail.com>
2024-09-02 01:15:21 +0300

    task 2

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+"""Task 2. Hypermedia pagination"""
+import csv
+import math
+from typing import List
+from typing import Tuple
+from typing import Dict
+from typing import Any
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """should return a tuple of size two containing a start index and
+    an end index corresponding to the range of indexes to return in a
+    list for those particular pagination parameters."""
+
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+
+    return start_index, end_index
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """should return a list of rows"""
+        assert isinstance(
+            page, int) and page > 0, "page must be positive int"
+        assert isinstance(
+          page_size, int) and page_size > 0, "page_size must be positive int"
+
+        start, end = index_range(page, page_size)
+        data = self.dataset()
+
+        if start >= len(data):
+            return []
+
+        return data[start:end]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Any]:
+        """
+        Return a dictionary with hypermedia pagination information.
+        """
+        data = self.get_page(page, page_size)
+        total_items = len(self.dataset())
+        total_pages = math.ceil(total_items / page_size)
+
+        hypermedia_info = {
+            'page_size': len(data),
+            'page': page,
+            'data': data,
+            'next_page': page + 1 if page < total_pages else None,
+            'prev_page': page - 1 if page > 1 else None,
+            'total_pages': total_pages
+        }
+
+        return hypermedia_info
