68e0667999a69aafdfc51a22add3a02c4e45ba2a
Aughostinos <augustinos.nabil@gmail.com>
2024-09-10 18:50:56 +0300

    task0

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+0x02. i18n
+Task 0. Basic Flask app
+"""
+from flask import request
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('0-index.html', language='en')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
