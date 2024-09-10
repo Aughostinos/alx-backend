3fee430543dd5b770d98d38d55ff716efe483a7b
Aughostinos <augustinos.nabil@gmail.com>
2024-09-11 02:06:06 +0300

    task 1

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+0x02. i18n
+Task 1. Basic Babel setup
+"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+class Config:
+    """configure available languages in our ap"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+# Instantiate Babel
+babel = Babel(app)
+
+@app.route('/')
+def home():
+    """home route"""
+    return render_template('1-index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
